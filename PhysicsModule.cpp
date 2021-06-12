21695d0aae315d6436273da262c8e515329beb99
Kashtangens <kashtan_kashtanow@mail.ru>
2021-06-12 03:53:40 +0300

    Collisions between entities in progress

--- PhysicsModule.cpp
+++ PhysicsModule.cpp
@@ -91,26 +91,26 @@ void CheckMapCollision(Entity *entity1, MapManager &mapManager, Map &map)
 
 void CheckCollision(Entity* entity1, Entity* entity2)
 {
-    // Проверку коллизий сделай, примерно как в карте, чтобы можно было направления указывать
-    if (entity1->GetRect().GetRect().y + entity1->GetRect().GetRect().h + entity1->GetDy() <= entity2->GetRect().GetRect().y + entity2->GetDy())
-    {
-        return;
-    }
-    if (entity2->GetRect().GetRect().y + entity2->GetRect().GetRect().h + entity2->GetDy() <= entity1->GetRect().GetRect().y + entity2->GetDy())
-    {
-        return;
-    }
-    if (entity1->GetRect().GetRect().x + entity1->GetRect().GetRect().w + entity1->GetDx() <= entity2->GetRect().GetRect().x + entity2->GetDx())
-    {
-        return;
-    }
-    if (entity2->GetRect().GetRect().x + entity2->GetRect().GetRect().w + entity2->GetDx() <= entity1->GetRect().GetRect().x + entity1->GetDx())
-    {
-        return;
-    }
-    printf("COLLISION\n");
-    entity1->ReactOnCollisionWithEntity(entity2, 1, 1);
-    entity2->ReactOnCollisionWithEntity(entity1, 1, 1);
+    // // Проверку коллизий сделай, примерно как в карте, чтобы можно было направления указывать
+    // if (entity1->GetRect().GetRect().y + entity1->GetRect().GetRect().h + entity1->GetDy() <= entity2->GetRect().GetRect().y + entity2->GetDy())
+    // {
+    //     return;
+    // }
+    // if (entity2->GetRect().GetRect().y + entity2->GetRect().GetRect().h + entity2->GetDy() <= entity1->GetRect().GetRect().y + entity2->GetDy())
+    // {
+    //     return;
+    // }
+    // if (entity1->GetRect().GetRect().x + entity1->GetRect().GetRect().w + entity1->GetDx() <= entity2->GetRect().GetRect().x + entity2->GetDx())
+    // {
+    //     return;
+    // }
+    // if (entity2->GetRect().GetRect().x + entity2->GetRect().GetRect().w + entity2->GetDx() <= entity1->GetRect().GetRect().x + entity1->GetDx())
+    // {
+    //     return;
+    // }
+    // printf("COLLISION\n");
+    // entity1->ReactOnCollisionWithEntity(entity2, 1, 1);
+    // entity2->ReactOnCollisionWithEntity(entity1, 1, 1);
 
 }
 //-------------------------------------------------------------------------------
